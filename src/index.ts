9e2232a0bea34555ff24bda0d9a3770679864827
hydratedgorilla <79025747+shanekizito@users.noreply.github.com>
2024-12-29 19:35:45 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,7 @@ const PORT = process.env.PORT || 5001;
 
 // initialize ngrok using the following function
 
-(async function() {
-  console.log("Initializing Ngrok tunnel...");
 
-  // Initialize ngrok using auth token and hostname
-  const url = await ngrok.connect({
-      proto: "http",
-      // Your authtoken if you want your hostname to be the same everytime
-      authtoken: process.env.ngrokauth,
-      // Your hostname if you want your hostname to be the same everytime
-      hostname: "",
-      // Your app port
-      addr: PORT,
-   
-  });
-
-  console.log(`Listening on url ${url}`);
-  console.log("Ngrok tunnel initialized!");
-})();
 
 //middlewares
 app.use(express.json());
