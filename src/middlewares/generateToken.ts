d2e6f1128111e6820473bfc9e4d3753b387d1e51
hydratedgorilla <79025747+shanekizito@users.noreply.github.com>
2025-01-03 13:36:00 +0300

    Update generateToken.ts

--- src/middlewares/generateToken.ts
+++ src/middlewares/generateToken.ts
@@ -13,7 +13,7 @@ export const generateToken = async (
   const CONSUMER_KEY = process.env.MPESA_CONSUMER_KEY as string;
   const CONSUMER_SECRET = process.env.MPESA_CONSUMER_SECRET as string;
   const URL =
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
   const auth = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString(
     "base64"
