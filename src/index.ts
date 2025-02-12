3f83248bfba523a666632ec14544c2efc083187f
hydratedgorilla <79025747+shanekizito@users.noreply.github.com>
2025-02-12 22:50:04 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/lipaRoute";
-import { initializeNgrok } from "../utils/ngrokManager";
 import serverless from "serverless-http";
 
 dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5001;
-
-(async () => {
-  // Initialize Ngrok and get the tunnel URL
-  const tunnelUrl = await initializeNgrok(PORT);
-  console.log(`Ngrok tunnel is available at: ${tunnelUrl}`);
-})();
 
 // Middleware setup
 app.use(express.json());
