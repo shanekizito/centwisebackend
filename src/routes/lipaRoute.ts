efb4ddde3d8c450fa3a3bb7ba3b63413e16abeb4
hydratedgorilla <79025747+shanekizito@users.noreply.github.com>
2025-02-12 22:58:30 +0300

    Update lipaRoute.ts

--- src/routes/lipaRoute.ts
+++ src/routes/lipaRoute.ts
@@ -6,9 +6,8 @@ import { paymentCallback } from "../controllers/callbackController";
 const router = express.Router();
 
 // Middleware chain: Generate token first, then handle STK push
-router.post("/stkpush", handleStkPush);
+router.post("/stkpush", generateToken, handleStkPush);
 router.post("/payment-callback", generateToken,paymentCallback);
-
 
 
 
